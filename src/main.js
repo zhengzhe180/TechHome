d4bc19b4e7fb169798458ef89010231019c09054
zz <zhengzhe180@outlook.com>
2026-02-06 14:51:44 +0800

    deep-chat实现聊天界面(非流式)

--- src/main.js
+++ src/main.js
@@ -2,43 +2,41 @@ import Vue from 'vue'
 
 import Cookies from 'js-cookie'
 
-import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import 'normalize.css/normalize.css'
 
 import Element from 'element-ui'
 import './styles/element-variables.scss'
 
-import '@/styles/index.scss' // global css
+import '@/styles/index.scss'
+
+// 1. 建议将 deep-chat 的引入放在 Vue 挂载前即可
+import 'deep-chat'
 
 import App from './App'
 import store from './store'
 import router from './router'
 
-import i18n from './lang' // internationalization
-import './icons' // icon
-import './permission' // permission control
-import './utils/error-log' // error log
+import i18n from './lang'
+import './icons'
+import './permission'
+import './utils/error-log'
 
-import * as filters from './filters' // global filters
+import * as filters from './filters'
 
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
 if (process.env.NODE_ENV === 'production') {
   const { mockXHR } = require('../mock')
   mockXHR()
 }
 
+// 2. 关键配置：确保 Vue 忽略自定义 Web Component 标签
+// 你使用的正则 [/^deep-/] 是正确的，能匹配 <deep-chat>
+Vue.config.ignoredElements = [/^deep-/]
+
 Vue.use(Element, {
-  size: Cookies.get('size') || 'medium', // set element-ui default size
+  size: Cookies.get('size') || 'medium',
   i18n: (key, value) => i18n.t(key, value)
 })
 
-// register global utility filters
 Object.keys(filters).forEach(key => {
   Vue.filter(key, filters[key])
 })
